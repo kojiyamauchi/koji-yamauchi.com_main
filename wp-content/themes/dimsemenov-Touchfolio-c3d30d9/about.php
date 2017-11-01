3d2009eb47a9cde59cb29746e16b41c722f86446
kojiyamauchi <all.the.small.things@me.com>
2017-11-01 22:29:56 +0900

    UpDate about.php

--- wp-content/themes/dimsemenov-Touchfolio-c3d30d9/about.php
+++ wp-content/themes/dimsemenov-Touchfolio-c3d30d9/about.php
@@ -18,7 +18,7 @@ Template Name: About Page
 WEB製作はWEBデザイン・設計から実装・コーディング・プログラミングまで承る事が可能です。<br>
 主な使用言語・ツールはHTML / HTML5 / CSS / CSS3 / Sass / Material Design Lite / JavaScript / WebGL / Riot.js / jQuery / gulp / GitHub / WordPress / <span class="lineThrough">PHP</span> / Photoshop / Illustrator / GitHub Atom & Emmet.<br>
 <br>
-Next Task => React・Vue.js・D3.js・Adobe XD.<br>
+Next Task => Vue.js・D3.js・Adobe XD・React.<br>
 <br>
 見る人間が長く観れる・使う人間が長く使えるサイト制作を心掛けております。<br>そして、そこに紐付けできる様なデザイン・構造設計・実装・意味のあるインタラクティブなモーション・アニメーション制作を強く意識して取り組みます。<br><br>
 WordPress等のCMSサイト構築・デザインのみ・コーディングのみ・プログラミングのみの案件も承る事が可能ですので、お気軽にご連絡下さい。<br>
